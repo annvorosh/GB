66d8771a9936839397dfc1778758df36d73bf67f
annvorosh <91416952+annvorosh@users.noreply.github.com>
2022-06-07 13:36:24 +0300

    Update Alg_L2_ex4.py

--- Alg_L2_ex4.py
+++ Alg_L2_ex4.py
@@ -3,14 +3,10 @@
 
 n = int(input('Введите количество элементов: '))
 
-# el = [1]
-# for i in range(n - 1):
-#     el.append(el[i] / (-2))
-# print(f'Сумма элементов  {el} = {sum(el)}')
 el = 1
 summ = 0
 for i in range(n):
     summ += el
     el /= (-2)
-print(summ)
+print(f'Сумма элементов = {summ}')
 
